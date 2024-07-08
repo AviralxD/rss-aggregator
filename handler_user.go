5adcbb400e7ec28ef35524213d4dde6e50fd97f5
Aviral M <aviralxd@gmail.com>
2024-07-08 23:01:09 +0530

    added api auth

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/AviralxD/rss-aggregator/internal/auth"
 	"github.com/AviralxD/rss-aggregator/internal/database"
 	"github.com/google/uuid"
 )
@@ -31,6 +32,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	})
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldnt create user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetApiKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByApiKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
 		return
 	}
 
